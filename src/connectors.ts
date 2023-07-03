90978fb040d9c31e925590a64076dfaa572bf5e2
Maxwell Hammad <maxhammad@gmail.com>
2023-07-03 04:01:26 -0400

    cleanup

--- src/connectors.ts
+++ src/connectors.ts
@@ -1,7 +1,5 @@
 const settings: { connectors: ConnectorDef[] } = require("../settings.json");
 import { BigQuery } from "./connectors/bigQuery";
-import { Databricks } from "./connectors/databricks";
-import { Snowflake } from "./connectors/snowflake";
 import { SqlService } from "./models/SqlService";
 
 export type ConnectorDef = {
@@ -21,27 +19,6 @@ export const connectors = settings.connectors.map((c) => {
   }
   let sqlService: SqlService;
   switch (c.type) {
-    case "databricks":
-      sqlService = new Databricks(
-        c.config.API_KEY,
-        c.config.DEFAULT_TABLE_NAME,
-        c.config.BASEURL,
-        c.config.WAREHOUSE_ID,
-        c.config.POLL_WAIT_MS,
-        c.config.POLL_TIMEOUT_MS
-      );
-      break;
-    case "snowflake":
-      sqlService = new Snowflake(
-        c.config.BASE_URL,
-        c.config.API_KEY,
-        c.config.POLL_WAIT_MS,
-        c.config.POLL_TIMEOUT_MS,
-        c.config.DEFAULT_TABLE_NAME,
-        c.config.DEFAULT_SCHEMA,
-        c.config.DEFAULT_DATABASE
-      );
-      break;
     case "bigquery":
       sqlService = new BigQuery(c.config.DATABASE, c.config.LOCATION);
       break;
